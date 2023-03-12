bb91e7ab34a4fa55d5cbeab09f9e8f2dab7b4d22
Vladyslav Tymchenko <vladyslavtymchenko@Vladyslavs-MacBook-Pro.local>
2023-03-12 16:13:32 +0200

    switched from react context to redux

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import PageNotFound from "./components/PageNotFound";
 import Store from "./components/Store";
-import CartContext from "./context/CartContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
@@ -16,32 +15,30 @@ function App() {
   return (
     <div className="App  ">
       <Provider store={store}>
-        <CartContext>
-          <BrowserRouter basename="/my-store">
-            <Cart />
-            <div className="flex flex-col justify-between h-screen">
-              <Header />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/store" element={<Store />} />
-                <Route path="/order" element={<OrderPage />} />
-                <Route path="*" element={<PageNotFound />} />
-              </Routes>
-              <Footer />
-            </div>
-            <ToastContainer
-              position="bottom-right"
-              autoClose={2000}
-              hideProgressBar={false}
-              newestOnTop={true}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              pauseOnHover
-              theme="light"
-            />
-          </BrowserRouter>
-        </CartContext>
+        <BrowserRouter basename="/my-store">
+          <Cart />
+          <div className="flex flex-col justify-between h-screen">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/store" element={<Store />} />
+              <Route path="/order" element={<OrderPage />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+          </div>
+          <ToastContainer
+            position="bottom-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={true}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            pauseOnHover
+            theme="light"
+          />
+        </BrowserRouter>
       </Provider>
     </div>
   );
